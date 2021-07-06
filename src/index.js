f5ae0fd29682cd8e1a3ae906d92bee074d8bd627
akdavila2 <anakarina.daviladavila@gmail.com>
2021-07-05 22:24:07 -0500

    8 commit optimizacion del archivo index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,31 @@
 //La sentencia import se usa para importar funciones que han sido exportadas desde un módulo externo.
 import { cipher } from './cipher.js';
-console.log(cipher);
-let code = document.getElementById('btnCode');
-let decode = document.getElementById('btnDecode');
-let encryptedMessage = document.getElementById('encryptedMessage');
-let clear = document.getElementById('btnRemove');
+const code = document.getElementById('btnCode');
+const decode = document.getElementById('btnDecode');
+const encryptedMessage = document.getElementById('encryptedMessage');
+const clean = document.getElementById('btnRemove');
+const message = document.getElementById('message');
+const positions = document.getElementById('positions');
 
 code.addEventListener('click', (event) => {
-    console.log('sirvo')
-        // Cancela el evento si este es cancelable, 
-        // sin detener el resto del funcionamiento del evento, es decir, 
-        // puede ser llamado de nuevo.
+    // Cancela el evento si este es cancelable, sin detener el resto del funcionamiento del evento, es decir, 
+    // puede ser llamado de nuevo.
     event.preventDefault();
-    console.log('sirvoCifrar');
-    let message = document.getElementById('message').value;
-    message = message.toUpperCase();
-    console.log(message);
-    let positions = document.getElementById('positions').value;
-    console.log('Segunda ronda: estoy aqui')
-    const result = cipher.encode(positions, message);
-    console.log(result, 'segunda ronda result', encryptedMessage, encryptedMessage.innerHTML);
+    let offset = positions.value;
+    const result = cipher.encode(offset, message.value.toUpperCase());
     encryptedMessage.innerHTML = result;
 });
 
 decode.addEventListener('click', (event) => {
-    console.log('sirvoDescifrar')
     event.preventDefault();
-    let message = document.getElementById('message').value;
-    message = message.toUpperCase();
-    console.log(message);
-    let positions = document.getElementById('positions').value;
-    encryptedMessage.innerHTML = cipher.decode(positions, message);
+    let offset = positions.value;
+    const result = cipher.decode(offset, message.value.toUpperCase());
+    encryptedMessage.innerHTML = result;
 });
 
-clear.addEventListener('click', (event) => {
-    console.log('sirvoLimpiar')
+clean.addEventListener('click', (event) => {
     event.preventDefault();
-    document.getElementById('message').value = '';
-    document.getElementById('positions').value = '';
-    document.getElementById('encryptedMessage').value = '';
+    message.value = '';
+    positions.value = '';
+    encryptedMessage.value = '';
 });
